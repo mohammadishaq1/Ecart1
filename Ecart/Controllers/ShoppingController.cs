0ceb8b8bbf0fdfa8e21389bf0152cfbc36a76de4
Mohammad Ishaq <info@ishaq.io>
2020-02-11 12:52:32 -0600

    Added shopping cart

---
+++ Ecart/Controllers/ShoppingController.cs
@@ -0,0 +1,42 @@
+﻿using Ecart.Models;
+using Ecart.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecart.Controllers
+{
+    public class ShoppingController : Controller
+    {
+
+        private EcartDBEntities objEcartDBEntities;
+        public ShoppingController()
+        {
+
+            objEcartDBEntities = new EcartDBEntities();
+        }
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            IEnumerable<ShoppingViewModel> listOfShoppingViewModels = (from objItem in objEcartDBEntities.Items
+                                                                       join
+                                                                       objCate in objEcartDBEntities.Categories
+                                                                       on objItem.CategoryID equals objCate.CategoryID
+                                                                       select new ShoppingViewModel()
+                                                                       {
+                                                                           ImagePath=objItem.ImagePath,
+                                                                           ItemName=objItem.ItemName,
+                                                                           Description=objItem.Description,
+                                                                           ItemPrice=objItem.ItemPrice,
+                                                                           ItemID=objItem.ItemID,
+                                                                           Category=objCate.CategoryName,
+                                                                           ItemCode=objItem.ItemCode
+                                                                       }
+
+                                                                       ).ToList();
+            return View(listOfShoppingViewModels);
+        }
+    }
+}
---
+++ Ecart/ViewModel/ShoppingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecart.ViewModel
+{
+    public class ShoppingViewModel
+    {
+        public Guid ItemID { get; set; }
+        public string ItemName { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+        public string ItemCode { get; set; }
+        public string Category { get; set; }
+    }
+}
