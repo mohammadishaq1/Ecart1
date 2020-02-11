31dd347970ceb32aa067ad558b78e87093325225
Mohammad Ishaq <info@ishaq.io>
2020-02-11 09:50:52 -0600

    Add project files.

---
+++ Ecart/Controllers/ItemController.cs
@@ -0,0 +1,53 @@
+﻿using Ecart.Models;
+using Ecart.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecart.Controllers
+{
+    public class ItemController : Controller
+    {
+        private EcartDBEntities objEcartDBEntities;
+        public ItemController()
+        {
+            objEcartDBEntities = new EcartDBEntities();
+        }
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objEcartDBEntities.Categories
+                                                       select new SelectListItem()
+                                                       {
+                                                           Text = objCat.CategoryName,
+                                                           Value = objCat.CategoryID.ToString(),
+                                                           Selected = true
+                                                       });
+
+            return View(objItemViewModel);
+        }
+        [HttpPost]
+        public JsonResult Index(ItemViewModel objItemViewModel)
+        {
+            string NewImage = Guid.NewGuid() + Path.GetExtension(objItemViewModel.ImagePath.FileName);
+            objItemViewModel.ImagePath.SaveAs(Server.MapPath("~/Images/" + NewImage));
+
+            Item objItem = new Item();
+            objItem.ImagePath = "~/Images/" + NewImage;
+            objItem.CategoryID = objItemViewModel.CategoryID;
+            objItem.Description = objItemViewModel.Description;
+            objItem.ItemCode = objItemViewModel.ItemCode;
+            objItem.ItemID = Guid.NewGuid();
+            objItem.ItemName = objItemViewModel.ItemName;
+            objItem.ItemPrice = objItemViewModel.ItemPrice;
+            objEcartDBEntities.Items.Add(objItem);
+            objEcartDBEntities.SaveChanges();
+
+            return Json(new { success = true, Message = "Item Added" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Ecart/ViewModel/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecart.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public int CategoryID { get; set; }
+        public string CategoryCode { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
